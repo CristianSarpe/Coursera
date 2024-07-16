8202a102f061eb6560e2ad28abe5d08718ad1fab
Cristian Șarpe <cristian@Cristians-MacBook-Air.local>
2024-07-16 12:32:00 +0300

    first commit

--- little-lemon/src/Main.js
+++ little-lemon/src/Main.js
@@ -1,1 +1,23 @@
-return (React)
+<html>
+  <head>
+<Routes> 
+    <Route path="/" element={<HomePage />}></Route>
+    <Route path="/booking" element={<BookingPage />}></Route>
+</Routes>
+</head>
+<body>
+  <h1>Welcome</h1>
+  <h2> to Little Lemon Restaurant</h2>
+  <p>Find all you want at the best quality and prices</p>
+
+  <script>
+    function updateTimes {
+      
+    }
+
+function initializeTimes {
+      
+    }
+  </script>
+</body>
+</html>
--- little-lemon/src/Nav.js
+++ little-lemon/src/Nav.js
@@ -1,1 +1,8 @@
-return (React)
+<html>
+<body> 
+  <button>Menu</button>
+  <button>Reserve</button>
+  <button>Contacts</button>
+  <button>About us</button>
+</body>
+</html>
